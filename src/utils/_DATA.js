8ad67101205b06e84e38a54b0bd31fe0ac448814
Yousef Majeed <uaso0of056@gmail.com>
2020-04-29 15:13:27 +0300

    fix issues in _data

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -8,6 +8,7 @@ let users = {
             "6ni6ok3ym7mf1p33lnez": "optionTwo",
             "am8ehyc8byjqgar0jgpub9": "optionTwo",
             "loxhs1bqm25b708cmbf3g": "optionOne"
+
         },
         questions: ["8xf0y6ziyjacvozdd253nd", "8xf0y6ziyjacaozdd253nd"]
     },
@@ -187,41 +188,41 @@ function formatQuestion({ optionOneText, optionTwoText, author }) {
 
 export function _saveQuestion(question) {
     return new Promise((res, rej) => {
-        const authedUser = question.author;
+        const authUser = question.author;
         const formattedQuestion = formatQuestion(question);
 
         setTimeout(() => {
             questions = {
                 ...questions,
                 [formattedQuestion.id]: formattedQuestion
-            }
+            };
 
             users = {
                 ...users,
-                [authedUser]: {
-                    ...users[authedUser],
-                    questions: users[authedUser].questions.concat([formattedQuestion.id])
+                [authUser]: {
+                    ...users[authUser],
+                    questions: users[authUser].questions.concat([formattedQuestion.id])
                 }
-            }
+            };
 
-            res(formattedQuestion)
-        }, 1000)
-    })
+            res(formattedQuestion);
+        }, 1000);
+    });
 }
 
-export function _saveQuestionAnswer({ authedUser, qid, answer }) {
+export function _saveQuestionAnswer({ authUser, qid, answer }) {
     return new Promise((res, rej) => {
         setTimeout(() => {
             users = {
                 ...users,
-                [authedUser]: {
-                    ...users[authedUser],
+                [authUser]: {
+                    ...users[authUser],
                     answers: {
-                        ...users[authedUser].answers,
+                        ...users[authUser].answers,
                         [qid]: answer
                     }
                 }
-            }
+            };
 
             questions = {
                 ...questions,
@@ -229,12 +230,12 @@ export function _saveQuestionAnswer({ authedUser, qid, answer }) {
                     ...questions[qid],
                     [answer]: {
                         ...questions[qid][answer],
-                        votes: questions[qid][answer].votes.concat([authedUser])
+                        votes: questions[qid][answer].votes.concat([authUser])
                     }
                 }
-            }
+            };
 
-            res()
-        }, 500)
-    })
+            res();
+        }, 500);
+    });
 }
